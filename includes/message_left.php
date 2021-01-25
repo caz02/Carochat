cbbe1ba873bdaedaf284cda9bb4cd6086ec7c57c
Caroline <76717163+caz02@users.noreply.github.com>
2021-01-25 02:54:41 +0000

    Add files via upload

---
+++ includes/message_left.php
@@ -0,0 +1,7 @@
+<div id='message_left'>
+<div></div>
+	<img src='$image'>
+	<b>$row->username</b><br>
+	This is a test message<br>
+	<span style='font-size:11px;color:#999;'>20 Jan 2020 10:00 am<span>
+</div>
